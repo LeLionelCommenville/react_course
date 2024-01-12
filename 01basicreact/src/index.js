2fb7ad6fa3c2e49fcd15de3dd756ff5fa544dbf6
Lionel Commenvillle <lionelcommenville@gmail.com>
2024-01-12 18:11:25 +0100

    create and clean a basic react app

---
+++ 01basicreact/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <App />
+);
+
+https://youtu.be/Bvwq_S0n2pk?si=0h-7QK5XdBX-RIEr&t=3945
