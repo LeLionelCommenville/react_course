4b961eddef7140d9e2c464ed72f777ce63743ff9
Lionel Commenvillle <lionelcommenville@gmail.com>
2024-01-13 17:10:05 +0100

    create a counter component

--- 02counter/src/App.js
+++ 02counter/src/App.js
@@ -1,9 +1,27 @@
+import { useState } from 'react'
+
 import './App.css';
 
 function App() {
+  
+  
+  const [counter, setCounter] = useState(0)
+
+  const addValue = () => {
+    setCounter(counter + 1);
+  }
+  
+  const substractValue = () => {
+    setCounter(counter - 1);
+  }
+
   return (
     <>
-     
+      <h1>React course counter</h1>
+      <h2>Counter value : {counter}</h2>
+      <button onClick={addValue}>Add value</button> {" "}
+      <button onClick={substractValue}>remove value</button>
+      <p>footer: {counter}</p> 
     </>  
   );
 }
