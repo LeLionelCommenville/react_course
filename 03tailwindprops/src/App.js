917258eb1abab449250f7faafccbcd831b9e606a
Lionel Commenvillle <lionelcommenville@gmail.com>
2024-01-14 18:28:18 +0100

    create a card component for testing props

--- 03tailwindprops/src/App.js
+++ 03tailwindprops/src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Card from './components/Card';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <Card username="Lionel"/>
+      <Card username="Alexandre" post="Software Enginer"/>
+    </>
   );
 }
 
