19495039ceb58f2a097176ef7b30f69b4778c5c1
Lionel Commenvillle <lionelcommenville@gmail.com>
2024-01-16 17:25:24 +0100

    just fix the label

--- 06currencyConverter/src/components/InputBox.jsx
+++ 06currencyConverter/src/components/InputBox.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useId} from "react";
 
 function InputBox({
     label,
@@ -11,14 +11,16 @@ function InputBox({
     currencyDisabled = false,
     className = "",
 }) {
-
+    const id = useId()    
     return(
         <div className={`bg-white p-3 rounded-lg text-sm flex 
         ${className} `}>
             <div className='w-1-2'>
-                <label className='text-black/40 mb-2 
+                <label htmlFor={id} className='text-black/40 mb-2 
                 inline-block'>{label}</label>
-                <input type="number"
+                <input 
+                id={id}
+                type="number"
                 className='outline-none w-full bg-transparent py-1.5'
                 placeholder='Amount'
                 disabled={amountDisabled}
