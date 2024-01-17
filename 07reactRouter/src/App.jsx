bc0d56aef7ddf405b1017705404f73f9d25d018d
Lionel Commenvillle <lionelcommenville@gmail.com>
2024-01-17 18:52:45 +0100

    create a project to testing react router dom

---
+++ 07reactRouter/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='bg-green-300'>React router dom</h1>
+    </>
+  )
+}
+
+export default App
---
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+import { useLoaderData } from "react-router-dom"
+
+function Github() {
+
+  const data = useLoaderData()
+  return (
+    <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github followers: {data.followers}
+    <img src={data.avatar_url} width={300} alt="" />
+    </div>
+  )
+}
+
+export default Github
+
+export const githubInfoLoader = async () => {
+  const response = await fetch('https://api.github.com/users/hiteshchoudhary')
+  return response.json()
+}
